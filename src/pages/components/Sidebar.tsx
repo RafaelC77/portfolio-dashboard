d6746aeb4e9a229d7787dbb3c5e15d306152e555
Rafael Guimaraes <rcarvalhobsb@gmail.com>
2022-10-01 14:27:34 -0300

    feat: add avatar to sidebar

--- src/pages/components/Sidebar.tsx
+++ src/pages/components/Sidebar.tsx
@@ -1,18 +1,32 @@
 import {
+  Avatar,
   Flex,
   Heading,
   Link,
   List,
   ListIcon,
   ListItem,
+  Text,
 } from "@chakra-ui/react";
 import { BsHouseFill, BsPeopleFill, BsBox } from "react-icons/bs";
 import NextLink from "next/link";
 
 export function Sidebar() {
   return (
-    <Flex flexDir="column" h="100%" w={200} align="center" py={10}>
-      <Heading mb={24}>Dash</Heading>
+    <Flex flexDir="column" h="100%" w={240} align="center" py={10}>
+      <Heading mb={12}>Dash</Heading>
+
+      <Flex direction="column" align="center" mb={12}>
+        <Avatar
+          size="xl"
+          name="Rafael Carvalho"
+          src="https://github.com/rafaelc77.png"
+        />
+        <Text fontWeight="bold" mt={2}>
+          Rafael Carvalho
+        </Text>
+        <Text fontSize="small">rcarvalhobsb@gmail.com</Text>
+      </Flex>
 
       <nav>
         <List spacing={4}>
