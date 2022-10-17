f0205a34ce317c349771f1565c1fe80f7f6a2534
Rafael Guimaraes <rcarvalhobsb@gmail.com>
2022-10-17 19:34:05 -0300

    feat: change sign in button color

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -90,7 +90,7 @@ export default function Home() {
 
         <Button
           type="submit"
-          colorScheme="blue"
+          colorScheme="green"
           mt={8}
           isLoading={formState.isSubmitting}
         >
